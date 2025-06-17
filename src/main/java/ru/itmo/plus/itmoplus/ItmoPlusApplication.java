aeb17335feaeea3efb3edb8a90dd5ba71c859007
ramil <rm.tj.777@gmail.com>
2025-06-17 22:18:34 +0300

    init

---
+++ src/main/java/ru/itmo/plus/itmoplus/ItmoPlusApplication.java
@@ -0,0 +1,13 @@
+package ru.itmo.plus.itmoplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItmoPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItmoPlusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/itmo/plus/itmoplus/ItmoPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itmo.plus.itmoplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItmoPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
